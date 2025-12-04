4cbbd79544c3eedc4191f72f1ca8b13666252e2b
Lindiwe Mfene <limfenejhb024@student.wethinkcode.co.za>
2025-12-05 00:10:32 +0200

    basic login will ad UI later

--- lib/Presentation/login_page_screen.dart
+++ lib/Presentation/login_page_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart' as auth;
-import 'package:firebase_ui_auth/firebase_ui_auth.dart' as ui;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'login_view_model.dart';
+import 'home_screen.dart';
 
-class SignInScreenWrapper extends StatelessWidget {
- 
+
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({
+    super.key,
+  });
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("FitQuest Login"),)
+      ,
+      body: Padding(padding:  const EdgeInsets.all(16.0),
+      child: Column(children: [TextField(controller: emailController,decoration: InputDecoration(labelText: "Email"),)
+      ,const SizedBox(height: 16),
+                  TextField(
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                    ),
+                  ),],),)
+
+      
+    );
+  }
 }
