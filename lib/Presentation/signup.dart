efcf3112629e2a3a02c8d0ed9ea44e2a0f18c945
Lindiwe Mfene <limfenejhb024@student.wethinkcode.co.za>
2025-12-05 03:58:17 +0200

    sign up page

---
+++ lib/Presentation/signup.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'signup_viewmodel.dart';
+import 'home_screen.dart';
+import 'login_page_screen.dart';
+
+class SignupScreen extends StatelessWidget {
+  SignupScreen({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  InputDecoration _buildInputDecoration(String label) {
+    return InputDecoration(
+      labelText: label,
+      labelStyle: const TextStyle(color: Colors.white),
+      filled: true,
+      fillColor: const Color.fromARGB(255, 63, 60, 60),
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(11)),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(11),
+        borderSide: const BorderSide(color: Colors.white),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(11),
+        borderSide: const BorderSide(color: Colors.deepPurpleAccent, width: 2),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => SignupViewModel(),
+      child: Consumer<SignupViewModel>(
+        builder: (context, viewModel, child) {
+          return Scaffold(
+            body: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.asset(
+                    'asset/images/fitnesslogin.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Center(
+                  child: SingleChildScrollView(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 400),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 80),
+                          const Text(
+                            "FitQuest Sign Up",
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 50),
+                          TextField(
+                            controller: emailController,
+                            decoration: _buildInputDecoration('Email'),
+                          ),
+                          const SizedBox(height: 16),
+                          TextField(
+                            controller: passwordController,
+                            obscureText: true,
+                            decoration: _buildInputDecoration('Password'),
+                          ),
+                          const SizedBox(height: 16),
+                          TextField(
+                            controller: confirmPasswordController,
+                            obscureText: true,
+                            decoration: _buildInputDecoration(
+                              'Confirm Password',
+                            ),
+                          ),
+                          const SizedBox(height: 24),
+                          if (viewModel.errorMessage != null)
+                            Text(
+                              viewModel.errorMessage!,
+                              style: const TextStyle(color: Colors.red),
+                            ),
+                          const SizedBox(height: 16),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: () async {
+                                final user = await viewModel.signup(
+                                  emailController.text,
+                                  passwordController.text,
+                                  confirmPasswordController.text,
+                                );
+                                if (user != null) {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (_) => const HomeScreen(),
+                                    ),
+                                  );
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color.fromARGB(
+                                  255,
+                                  121,
+                                  61,
+                                  249,
+                                ),
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                textStyle: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              child: viewModel.isLoading
+                                  ? const CircularProgressIndicator(
+                                      color: Colors.white,
+                                    )
+                                  : const Text('Sign Up'),
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => LoginScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              'Already have an account? Login',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          const SizedBox(height: 40),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
