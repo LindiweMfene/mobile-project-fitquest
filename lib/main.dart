a55f4a9ba927016f1aa6747a164deaab4302379e
Lindiwe Mfene <limfenejhb024@student.wethinkcode.co.za>
2025-12-07 22:55:25 +0200

    changed main to have repo implementation direct injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mobile_project_fitquest/Presentation/Login_page_screen.dart';
+import 'package:mobile_project_fitquest/Data/run_repository_impl.dart';
+import 'package:mobile_project_fitquest/viewmodels/run_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:mobile_project_fitquest/Presentation/login_page_screen.dart';
+
+import 'package:mobile_project_fitquest/viewmodels/signup_viewmodel.dart'; // ✅ Import here
 import 'firebase_options.dart';
 
 void main() async {
@@ -14,12 +19,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FitQuest',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => RunViewModel(RunRepositoryImpl()), // ✅ Use here
+        ),
+        ChangeNotifierProvider(create: (_) => SignupViewModel()),
+      ],
+      child: MaterialApp(
+        title: 'FitQuest',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: LoginScreen(),
       ),
-      home: LoginScreen(), // <- no 'const' here
     );
   }
 }
